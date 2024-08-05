24e55b1222732d6b625bb34b93f74543f5286b3f
dnjsrn72 <wglee219@gmail.com>
2024-08-05 13:21:36 +0900

    Product string was modified.

--- STM32H723ZG_Bootloader/BTL_BOOT/USB_DEVICE/App/usbd_desc.c
+++ STM32H723ZG_Bootloader/BTL_BOOT/USB_DEVICE/App/usbd_desc.c
@@ -66,7 +66,7 @@
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "NANO TS"
 #define USBD_PID_HS     0x1234
-#define USBD_PRODUCT_STRING_HS     "Original DFU bootloader"
+#define USBD_PRODUCT_STRING_HS     "NANO TS bootloader"
 #define USBD_CONFIGURATION_STRING_HS     "DFU Config"
 #define USBD_INTERFACE_STRING_HS     "DFU Interface"
 
